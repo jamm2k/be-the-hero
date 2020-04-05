e90d54def0ab0e9b9e06c1941066ee70809ff4b1
Arthur Mello <j4mmbr@gmail.com>
2020-04-04 22:03:28 -0300

    Mobile do app.3

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity, Linking } from 'react-native';
+import { View, Image, Text, TouchableOpacity, Linking, FlatList } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import * as MailComposer from 'expo-mail-composer';
@@ -8,7 +8,7 @@ import styles from './styles';
 
 import logoImg from '../../assets/logo.png';
 
-export default function Detail(){
+export default function Detail() {
     const navigation = useNavigation();
     const route = useRoute();
     const incident = route.params.incident;
@@ -45,40 +45,49 @@ export default function Detail(){
                 </TouchableOpacity>
             </View>
 
-            <View style={styles.incident}>
-                <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-                <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf} </Text>
+            <FlatList
+                data={[1]}
+                style={styles.incidentList}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+                        <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
-                <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>{incident.title}</Text>
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
-                <Text style={styles.incidentProperty}>VALOR:</Text>
-                <Text style={styles.incidentValue}>
-                    {Intl.NumberFormat('pt-BR', { 
-                        style: 'currency', 
-                        currency: 'BRL' 
-                    }).format(incident.value)}
-                </Text>
-            </View>
+                        <Text style={styles.incidentProperty}>DESCRIÇÃO:</Text>
+                        <Text style={styles.incidentValue}>{incident.description}</Text>
 
-            <View style={styles.contactBox}>
-                <Text style={styles.heroTitle}>Salve o dia!</Text>
-                <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>
+                            {Intl.NumberFormat('pt-BR', { 
+                                style: 'currency', 
+                                currency: 'BRL' 
+                            }).format(incident.value)}
+                        </Text>
 
-                <Text style={styles.heroDescription}>Entre em contato:</Text>
+                        <View style={styles.contactBox}>
+                            <Text style={styles.heroTitle}>Salve o dia!</Text>
+                            <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
 
-                <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
-                        <Text style={styles.actionText}>WhatsApp:</Text>
-                    </TouchableOpacity>
+                            <Text style={styles.heroDescription}>Entre em contato:</Text>
 
-                    <TouchableOpacity style={styles.action} onPress={sendMail}>
-                        <Text style={styles.actionText}>Email:</Text>
-                    </TouchableOpacity>
-                </View>
+                            <View style={styles.actions}>
+                                <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
+                                    <Text style={styles.actionText}>WhatsApp:</Text>
+                                </TouchableOpacity>
 
-
-            </View>
+                                <TouchableOpacity style={styles.action} onPress={sendMail}>
+                                    <Text style={styles.actionText}>Email:</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                )}
+            />
         </View>
     );
 };
